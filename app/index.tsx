ab806f8c2881a9f488d436eadf7321c2e45b7f2c
gitongah <mbaabugitonga@gmail.com>
2024-09-28 00:09:56 -0700

    Added a components btn

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import beachImage from '@/assets/meditation-images/beach.webp';
 import { LinearGradient } from 'expo-linear-gradient'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import CustomButton from '@/components/CustomButton';
 
 const App = () => {
   return (
@@ -15,7 +16,7 @@ const App = () => {
         <LinearGradient
           className='flex-1 '
           colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
-            <SafeAreaView className='flex-1 px-1 justify-between'>
+            <SafeAreaView className='flex-1 mx-5 my-12 justify-between'>
             <View>
               <Text className='text-center text-white font-bold text-4xl'>
                 Simple Meditation
@@ -23,6 +24,9 @@ const App = () => {
               <Text className='text-center text-white text-regular text-2xl mt-3'>
                 Simplifying Meditation for Everyone.
               </Text>
+            </View>
+            <View>
+              <CustomButton onPress={()=> console.log('tap')} title='Get Started'/>
             </View>
 
 
