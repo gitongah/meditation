2376d2764b42eaffc13d304c1ff372f0e5c7780e
gitongah <mbaabugitonga@gmail.com>
2024-10-01 16:51:39 -0700

    Added the arfirmation btn on the index screen

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)/nature-meditate` | `/_sitemap` | `/nature-meditate`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/affirmation` | `/(tabs)/nature-meditate` | `/_sitemap` | `/affirmation` | `/nature-meditate`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
---
+++ app/(tabs)/affirmation.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Affrirmation = () => {
+  return (
+    <View>
+      <Text>Affrirmation</Text>
+    </View>
+  )
+}
+
+export default Affrirmation
